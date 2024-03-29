308ce7c7e6a3668df5f7a8c7dc0a827128c3384e
martinalupini <martinalupini11@gmail.com>
2024-03-29 13:50:14 +0100

    modified header

--- create_header.sh
+++ create_header.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-entry1=$( cat "/sys/module/the_reference_monitor/parameters/entry1" )
+
+entry1=$(cat "/sys/module/the_reference_monitor/parameters/entry1")
 entry2=$(cat "/sys/module/the_reference_monitor/parameters/entry2")
 entry3=$(cat "/sys/module/the_reference_monitor/parameters/entry3")
 entry4=$(cat "/sys/module/the_reference_monitor/parameters/entry4")
@@ -8,17 +9,12 @@ entry5=$(cat "/sys/module/the_reference_monitor/parameters/entry5")
 entry6=$(cat "/sys/module/the_reference_monitor/parameters/entry6")
 entry7=$(cat "/sys/module/the_reference_monitor/parameters/entry7")
 
+output="entries.h"
+
+content="#ifndef _ENTRIES_\n\n#define _ENTRIES_\n\n#define ENTRY1 $entry1\n#define ENTRY2 $entry2\n#define ENTRY3 $entry3\n#define ENTRY4 $entry4\n#define ENTRY5 $entry5\n#define ENTRY6 $entry6\n#define ENTRY7 $entry7\n\n#endif"
+
+
 cd user/syscallsCLI/lib/include
-echo "#ifndef _ENTRIES_" > entries.h
-echo " " >> entries.h
-echo "#define _ENTRIES_" >> entries.h
-echo " " >> entries.h
-echo "#define ENTRY1 $entry1" >> entries.h
-echo "#define ENTRY2 $entry2" >> entries.h
-echo "#define ENTRY3 $entry3" >> entries.h
-echo "#define ENTRY4 $entry4" >> entries.h
-echo "#define ENTRY5 $entry5" >> entries.h
-echo "#define ENTRY6 $entry6" >> entries.h
-echo "#define ENTRY7 $entry7" >> entries.h
-echo " " >> entries.h
-echo "#endif" >> entries.h
+echo -e "$content" > entries.h
+
+echo "il file header è stato scritto"
